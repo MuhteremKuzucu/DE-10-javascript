beb5efbb54363c9f2313a6b2d937a0b02baf28bd
MuhteremKuzucu <kuzucumuhterem@gmail.com>
2024-09-11 12:43:52 +0200

    derscsonu

---
+++ javascript/class-notes/JS-02-Operators/app.js
@@ -0,0 +1,44 @@
+
+/* ---------------------------------- */
+/*        ARİTMETİK OPERATÖRLER       */
+/* ---------------------------------- */
+
+
+const cola=50
+const cips=35
+const dondurma=20
+
+console.log("cola" + cips + dondurma);
+
+let toplam=cola + cips + dondurma
+
+console.log(toplam);//105
+
+
+//! 1 arttır
+
+toplam = toplam + 1
+
+console.log(toplam);//106
+
+toplam++
+
+console.log(toplam);//107
+
+//! 10 arttır
+
+toplam=toplam+10
+
+toplam+=10 //toplam=toplam+10 kısa yolu
+
+console.log(toplam);
+
+
+
+
+
+
+
+
+
+
